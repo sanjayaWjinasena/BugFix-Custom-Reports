01cc3d24de95b80ab3e93859b2ad5b934fa612ca
Sanjaya <sanjaya.w@jinasena.com.lk>
2026-09-10 10:37:52 +0530

    v17.0.0.0.9: Phase E views - ship view gaps for BugFix-Custom-Reports scope

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'BugFix - Custom - Reports',
-    'version': '17.0.0.0.8',
+    'version': '17.0.0.0.9',
     'summary': 'Studio custom-reports config models (x_custom_reports, tags, stages, x_configuration)',
     'author': 'Jinasena Agricultural Machinery (Pvt) Ltd.',
     'category': 'Extra Tools',
@@ -13,6 +13,9 @@
         'data/record_rules_gap.xml',
         'data/window_actions_gap.xml',
         'data/ir_defaults_gap.xml',
+        'views/x_configuration_e_views.xml',
+        'views/x_custom_reports_e_views.xml',
+        'views/x_custom_reports_stage_e_views.xml',
     ],
     'installable': True,
     'auto_install': False,
